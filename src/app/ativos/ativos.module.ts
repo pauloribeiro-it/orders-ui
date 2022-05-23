86de7f3843a04bec4d2c4975bddf9747dbef893b
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2022-05-23 08:51:54 -0300

    Adicionando validação

--- src/app/ativos/ativos.module.ts
+++ src/app/ativos/ativos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AtivosRoutingModule } from './ativos-routing.module';
 import { AtivosComponent } from './cadastro-ativos/ativos.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,10 @@ import { DropdownModule } from 'primeng/dropdown';
     AtivosRoutingModule,
     InputTextModule,
     InputNumberModule,
-    DropdownModule
+    DropdownModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AtivosModule { }
--- src/app/ativos/cadastro-ativos/ativos.component.ts
+++ src/app/ativos/cadastro-ativos/ativos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AtivosService } from '../ativos.service';
 import { TipoAtivo } from './TipoAtivo';
 @Component({
@@ -7,9 +8,16 @@ import { TipoAtivo } from './TipoAtivo';
   styleUrls: ['./ativos.component.css']
 })
 export class AtivosComponent implements OnInit {
-
+  formulario: FormGroup;
   tipos: TipoAtivo[] = [];
-  constructor(private ativosService: AtivosService) { }
+
+  constructor(private ativosService: AtivosService, private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      codigo: [null, Validators.required],
+      tipo: [null, Validators.required],
+      descricao: [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     this.ativosService.getTiposAtivos().then(
@@ -17,6 +25,10 @@ export class AtivosComponent implements OnInit {
           this.tipos = resposta.map((r: TipoAtivo) => new TipoAtivo(r.descricao, r.id));
       }
     );
+  }
+
+  salvar(): void{
+
   }
 
 }
